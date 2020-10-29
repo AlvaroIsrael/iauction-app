3c75b4e80de2eafd0b56876a030a8e6fc0716469
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-10-29 18:49:59 -0300

    chore: Added server files and custom error class.

---
+++ iauction-backend/src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import * as HttpStatus from 'http-status-codes';
+/*import routes from './routes';*/
+import AppError from './errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+/* app.use(routes); */
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  /* Minimun of global error handling ensuring our app will never let an unhandled exception break. */
+  return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
+
+export default app;
---
+++ iauction-backend/src/server.ts
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(3333, () => {
+  console.log('🔥 Server running on port 3333! 🔥');
+});
